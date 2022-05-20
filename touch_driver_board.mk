5567d48bacf8931096f6656d836042418514113f
Shashank Babu Chinta Venkata <quic_schintav@quicinc.com>
2022-05-20 15:37:55 -0700

    touch: add support to compile out touch modules
    Add support to selective compile out touch module based
    on compile flag.
    
    Change-Id: I0525b7664576740acfeb7febce12e666a9a6c7ba
    Signed-off-by: Shashank Babu Chinta Venkata <quic_schintav@quicinc.com>

--- touch_driver_board.mk
+++ touch_driver_board.mk
@@ -1,9 +1,16 @@
+TOUCH_DLKM_ENABLE := true
+ifeq ($(TARGET_KERNEL_DLKM_DISABLE), true)
+	ifeq ($(TARGET_KERNEL_DLKM_TOUCH_OVERRIDE), false)
+		TOUCH_DLKM_ENABLE := false
+	endif
+endif
 
-ifneq ($(TARGET_BOARD_AUTO),true)
-	ifeq ($(call is-board-platform-in-list,$(TARGET_BOARD_PLATFORM)),true)
-		BOARD_VENDOR_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/nt36xxx-i2c.ko \
-			$(KERNEL_MODULES_OUT)/goodix_ts.ko \
-			$(KERNEL_MODULES_OUT)/atmel_mxt_ts.ko \
-			$(KERNEL_MODULES_OUT)/dummy_ts.ko
+ifeq ($(TOUCH_DLKM_ENABLE),  true)
+	ifneq ($(TARGET_BOARD_AUTO),true)
+		ifeq ($(call is-board-platform-in-list,$(TARGET_BOARD_PLATFORM)),true)
+			BOARD_VENDOR_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/nt36xxx-i2c.ko \
+				$(KERNEL_MODULES_OUT)/goodix_ts.ko \
+				$(KERNEL_MODULES_OUT)/atmel_mxt_ts.ko
+		endif
 	endif
 endif
--- touch_driver_product.mk
+++ touch_driver_product.mk
@@ -1,5 +1,12 @@
+TOUCH_DLKM_ENABLE := true
+ifeq ($(TARGET_KERNEL_DLKM_DISABLE), true)
+	ifeq ($(TARGET_KERNEL_DLKM_TOUCH_OVERRIDE), false)
+		TOUCH_DLKM_ENABLE := false
+	endif
+endif
 
-PRODUCT_PACKAGES += $(KERNEL_MODULES_OUT)/nt36xxx-i2c.ko \
-	$(KERNEL_MODULES_OUT)/goodix_ts.ko \
-	$(KERNEL_MODULES_OUT)/atmel_mxt_ts.ko \
-	$(KERNEL_MODULES_OUT)/dummy_ts.ko
+ifeq ($(TOUCH_DLKM_ENABLE),  true)
+	PRODUCT_PACKAGES += $(KERNEL_MODULES_OUT)/nt36xxx-i2c.ko \
+		$(KERNEL_MODULES_OUT)/goodix_ts.ko \
+		$(KERNEL_MODULES_OUT)/atmel_mxt_ts.ko
+endif
