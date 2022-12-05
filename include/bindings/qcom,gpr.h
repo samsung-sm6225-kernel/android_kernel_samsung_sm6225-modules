7dd11fe70fdf682745cb9e1b131370831b9c50fb
Swarna Latha <quic_swarlath@quicinc.com>
2022-12-05 11:10:17 +0530

    include: bindings: Define GPR_DOMAIN_CC_DSP
    Add GPR_DOMAIN_CC_DSP in bindings and which is
    needed for audio device tree.
    
    Change-Id: I626b792f46b96c9e3778762bba01c1fc8049ee91
    Signed-off-by: Swarna Latha <quic_swarlath@quicinc.com>

--- include/bindings/qcom,gpr.h
+++ include/bindings/qcom,gpr.h
@@ -1,5 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 /* Copyright (c) 2020-2021, The Linux Foundation. All rights reserved. */
+/* Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.*/
 #ifndef __DT_BINDINGS_QCOM_GPR_H
 #define __DT_BINDINGS_QCOM_GPR_H
 
@@ -10,7 +11,8 @@
 #define GPR_DOMAIN_APPS		0x3
 #define GPR_DOMAIN_SDSP		0x4
 #define GPR_DOMAIN_CDSP		0x5
-#define GPR_DOMAIN_MAX		0x6
+#define GPR_DOMAIN_CC_DSP	0x6
+#define GPR_DOMAIN_MAX		0x7
 
 /* ADSP service IDs */
 #define GPR_SVC_ADSP_CORE	0x3
