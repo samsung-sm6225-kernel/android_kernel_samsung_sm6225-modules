3fd5861f0b238a1eec099fc7af48f16251f30846
Erin Yan <quic_xinyey@quicinc.com>
2023-03-23 14:59:54 +0800

    config: add configuration for sa8255
    Enable CONFIG_DIGITAL_CDC_RSC_MGR to dynamically enable/disable hw vote during
    LPM resume/suspend.
    
    Change-Id: Iadd2b1576bf01eb419029c89fb4b79c1c8f791bf

--- config/sa8255autoconf.h
+++ config/sa8255autoconf.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+/* Copyright (c) 2022-2023 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -24,3 +24,4 @@
 #define CONFIG_SND_SOC_SA8255_AUTO_SPF 1
 #define CONFIG_PINCTRL_LPI 1
 #define CONFIG_SND_SOC_WCD9XXX_V2 1
+#define CONFIG_DIGITAL_CDC_RSC_MGR 1
