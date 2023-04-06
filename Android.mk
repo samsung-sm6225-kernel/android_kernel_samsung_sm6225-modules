2a5dec7a81069d05e05df6069d120d12c3b6f2d6
Manikantan R <quic_manrav@quicinc.com>
2023-04-06 15:36:08 +0530

    audio-kernel: Voice over pcie makerules
    update makefile to support pcie variant
    
    Change-Id: Ibfaf635c15973ec36b837c7b83e3878df1693f6f
    signed-off-by: Manikantan R <quic_manrav@quicinc.com>

--- Android.mk
+++ Android.mk
@@ -301,7 +301,15 @@ LOCAL_MODULE_TAGS         := optional
 LOCAL_MODULE_DEBUG_ENABLE := true
 LOCAL_MODULE_PATH         := $(KERNEL_MODULES_OUT)
 include $(DLKM_DIR)/Build_external_kernelmodule.mk
-
+########################## PCIE DRIVER ################################
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES   := $(wildcard $(LOCAL_PATH)/**/*) $(wildcard $(LOCAL_PATH)/*)
+LOCAL_MODULE              := msm_pcm_pcie_dlkm.ko
+LOCAL_MODULE_KBUILD_NAME  := asoc/msm_pcm_pcie_dlkm.ko
+LOCAL_MODULE_TAGS         := optional
+LOCAL_MODULE_DEBUG_ENABLE := true
+LOCAL_MODULE_PATH         := $(KERNEL_MODULES_OUT)
+include $(DLKM_DIR)/Build_external_kernelmodule.mk
 ########################### LPASS-CDC CODEC  ###########################
 ifeq ($(call is-board-platform-in-list, kalama),true)
 include $(CLEAR_VARS)
