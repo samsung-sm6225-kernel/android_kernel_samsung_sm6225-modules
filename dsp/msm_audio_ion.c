fbf383cd6205577f09936c2f53fc7b21a131924c
Kogara Naveen Kumar <quic_nkogara@quicinc.com>
2023-05-17 16:30:23 +0530

    dsp: Fix regression introduced by change ID 4590003
    Change ID 4590003 reverts a fix added for skipping the
    reclaiming of hyp_assigned memory from TZ.
    There is an underlying limitation at TZ due to which reclaiming
    the memory causes crash.
    
    Adding the original fix through this change for unblocking PF.
    
    Change-Id: I86d016ddc9a672acfd766491c895a3f41de166bb

--- dsp/msm_audio_ion.c
+++ dsp/msm_audio_ion.c
@@ -708,8 +708,17 @@ void msm_audio_ion_crash_handler(void)
 			handle = msm_audio_fd_data->handle;
 			ion_data = dev_get_drvdata(msm_audio_fd_data->dev);
 			/*  clean if CMA was used*/
-			if (msm_audio_fd_data->hyp_assign)
+			/*
+			 * TODO: assigned memory to adsp, mdsp & sdsp cannot be reclaimed,
+			 * caused  by a known issue from TZ.
+			 * After TZ fixes the issue, the memory can have the common handling
+			 */
+			if (msm_audio_fd_data->hyp_assign) {
+				if (msm_audio_fd_data->ss_masks == (0x1|0x2|0x8)) {
+					continue;
+				}
 				msm_audio_hyp_unassign(msm_audio_fd_data);
+			}
 			if (handle)
 				msm_audio_ion_free(handle, ion_data);
 		}
